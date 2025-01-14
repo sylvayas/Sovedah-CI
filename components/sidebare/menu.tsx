88f3d4b6a781697ade19b28ff98b9784434f916c
AITECHCI <aitechci1@gmail.com>
2025-01-14 09:50:22 +0000

    Correction sidebare F

--- components/sidebare/menu.tsx
+++ components/sidebare/menu.tsx
@@ -1,146 +1,73 @@
-import TitleSection from '@/components/title-section';
-import React from 'react';
-import { BorderBeam } from "@/components/magicui/border-beam";
-import AnimatedGridPattern from "@/components/magicui/animated-grid-pattern";
+"use client";
+
 import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
+
+import { usePathname } from "next/navigation";
+
 import { cn } from "@/lib/utils";
-import Image from 'next/image'; // Import Image from next/image
+import { getMenuList } from "@/config/menu-list";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { CollapseMenuButton } from "@/components/sidebare/collapse-menu-button";
+import { Dispatch, SetStateAction } from "react";
 
-export default function DescriptionAllSpaces() {
-    return (
-        <section className="container min-h-[300px] py-14 relative">
-            <TitleSection title={"Nos différents espaces situés à Cocody"} />
 
-            <div className="relative gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-                <Image
-                    className="w-full"
-                    src="/images/coworking/Bureaux privés/Bureau confiance/img (1).jpg"
-                    alt="Bureau confiance"
-                    width={800} // Specify the width
-                    height={500} // Specify the height
-                />
-                <div className="mt-4 md:mt-0">
-                    <h2 className="mb-4 text-2xl md:text-4xl tracking-tight font-saudagar">Bureaux privés</h2>
-                    <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-                        Installez-vous et faites comme chez vous. La connexion internet est disponible et profitez de tout ce dont nous vous mettrons à disposition dans votre bureau. <br />
-                        En fonction de la forme de votre installation vous bénéficierez de différents services.
-                    </p>
-                    <Link
-                        className={cn(
-                            buttonVariants({ size: "sm" }),
-                            "max-w-52 gap-2 overflow-hidden whitespace-pre",
-                            "group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-primary hover:ring-offset-2",
-                        )}
-                        href={`/our_spaces/private_offices`}
-                    >
-                        <span className="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40" />
-                        <div className="flex items-center">
-                            <span className="ml-1 text-sm sm:text-md">Savoir plus</span>
-                        </div>
-                    </Link>
-                </div>
+export function Menu({ setOpen }: {
+  setOpen: Dispatch<SetStateAction<boolean>>
+}) {
+  const pathname = usePathname();
+  const menuList = getMenuList(pathname);
+
+  return (
+    <ScrollArea className="h-[calc(100vh-4rem)] pt-4 pb-10">
+      <nav className="flex flex-col space-y-2">
+        <div className="flex flex-col min-h-[calc(100vh-48px-36px-16px-32px)] lg:min-h-[calc(100vh-32px-40px-32px)] items-start space-y-1 px-2">
+          {menuList.map(({ groupLabel, menus }, index) => (
+            <div className={cn("w-full", groupLabel ? "pt-5" : "")} key={index}>
+              <p className="text-sm font-medium text-muted-foreground px-4 pb-2 max-w-[248px] truncate">
+                {groupLabel}
+              </p>
+              {menus.map(
+                ({ href, label, icon: Icon, active, submenus }, index) =>
+                  submenus.length === 0 ? (
+                    <div className="w-full" key={index}>
+                      <Button
+                        variant={active ? "default" : "ghost"}
+                        className="w-full justify-start h-10 mb-1"
+                        asChild
+                      >
+                        <Link href={href}>
+                          <span
+                            className={cn("mr-4")}
+                          >
+                            <Icon size={18} />
+                          </span>
+                          <p
+                            className={cn(
+                              "max-w-[200px] truncate translate-x-0 opacity-100"
+                            )}
+                          >
+                            {label}
+                          </p>
+                        </Link>
+                      </Button>
+                    </div>
+                  ) : (
+                    <div className="w-full" key={index}>
+                      <CollapseMenuButton
+                        icon={Icon}
+                        label={label}
+                        active={active}
+                        submenus={submenus}
+                        setOpen={setOpen}
+                      />
+                    </div>
+                  )
+              )}
             </div>
-
-            <div className="relative flex flex-col-reverse md:gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-                <div className="mt-4 md:mt-0">
-                    <h2 className="mb-4 text-2xl md:text-4xl tracking-tight font-saudagar">Open space</h2>
-                    <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-                        Retrouvez Novis à Villeneuve d&apos;Ascq, à proximité immédiate du Parc de la Haute Borne. <br />
-                        Novis Lille Villeneuve d&apos;Ascq prend place dans un complexe prime écoresponsable, labellisé WELL niveau Silver, à deux pas du Stade Pierre Mauroy, du centre commercial Héron Parc et des nombreuses boutiques du centre commercial V2.
-                    </p>
-                    <Link
-                        className={cn(
-                            buttonVariants({ size: "sm" }),
-                            "max-w-52 gap-2 overflow-hidden whitespace-pre",
-                            "group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-primary hover:ring-offset-2",
-                        )}
-                        href={`/our_spaces/open_space`}
-                    >
-                        <span className="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40" />
-                        <div className="flex items-center">
-                            <span className="ml-1 text-sm sm:text-md">Savoir plus</span>
-                        </div>
-                    </Link>
-                </div>
-                <Image
-                    className="w-full"
-                    src="/images/coworking/Espace Bulle Open space/img (1).jpg"
-                    alt="Open space"
-                    width={800}
-                    height={500}
-                />
-            </div>
-
-            <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-                <Image
-                    className="w-full"
-                    src="/images/coworking/Salle de réunion/img (1).jpg"
-                    alt="Salle de réunion"
-                    width={800}
-                    height={500}
-                />
-                <div className="mt-4 md:mt-0">
-                    <h2 className="mb-4 text-2xl md:text-4xl tracking-tight font-saudagar">Salle de réunion</h2>
-                    <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-                        Vous souhaitez pouvoir recevoir du monde afin d&apos;y organiser des réunions ? NOVIS coworking vous donne la possibilité de pouvoir louer cette salle de réunion moderne dans Abidjan <br />
-                        Que cela se fasse avec vos collaborateurs, vos prestataires et/ou vos clients, vous disposez de cet espace de <span className="font-bold">20 m2</span>. Ce lieu de réflexion peut accueillir <span className="font-bold">10 voir 14 personnes</span> si nécessité de rallonge.
-                    </p>
-                    <Link
-                        className={cn(
-                            buttonVariants({ size: "sm" }),
-                            "max-w-52 gap-2 overflow-hidden whitespace-pre",
-                            "group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-primary hover:ring-offset-2",
-                        )}
-                        href={`/our_spaces/meeting_room`}
-                    >
-                        <span className="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40" />
-                        <div className="flex items-center">
-                            <span className="ml-1 text-sm sm:text-md">Savoir plus</span>
-                        </div>
-                    </Link>
-                </div>
-            </div>
-
-            <div className="relative flex flex-col-reverse md:gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-                <div className="mt-4 md:mt-0">
-                    <h2 className="mb-4 text-2xl md:text-4xl tracking-tight font-saudagar">Espace commun</h2>
-                    <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-                        Les espaces commun ou espaces de vie, sont des espaces de grandes convivialités pouvant servir également à différents types d&apos;évènements comme <span className="font-bold">soirée networking, apéro business, expositions, ventes, talks-show, tournages poadcast, interviews etc…</span> <br />
-                        Ce lieu se prête à la réalisation de tous vos projets commerciaux, artistiques et de communication.
-                    </p>
-                    <Link
-                        className={cn(
-                            buttonVariants({ size: "sm" }),
-                            "max-w-52 gap-2 overflow-hidden whitespace-pre",
-                            "group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-primary hover:ring-offset-2",
-                        )}
-                        href={`/our_spaces/common_space`}
-                    >
-                        <span className="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 bg-white opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40" />
-                        <div className="flex items-center">
-                            <span className="ml-1 text-sm sm:text-md">Savoir plus</span>
-                        </div>
-                    </Link>
-                </div>
-                <Image
-                    className="w-full"
-                    src="/images/coworking/Espace commun/img (1).jpg"
-                    alt="Espace commun"
-                    width={800}
-                    height={500}
-                />
-            </div>
-
-            <AnimatedGridPattern
-                numSquares={50}
-                maxOpacity={0.1}
-                duration={2}
-                repeatDelay={1}
-                className={
-                    "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)] inset-x-0 w-full h-[100%]"
-                }
-            />
-        </section>
-    );
+          ))}
+        </div>
+      </nav>
+    </ScrollArea>
+  );
 }
--- config/menu-list.ts
+++ config/menu-list.ts
@@ -2,101 +2,102 @@ import { LucideIcon } from "lucide-react";
 import { Icons } from "@/components/icons";
 
 type Submenu = {
-  href?: string;  // Rendre href optionnel
-  label: string;
-  active: boolean;
+    href?: string; // Make href optional
+    label: string;
+    active: boolean;
 };
 
 type Menu = {
-  href: string;
-  label: string;
-  active: boolean;
-  icon: LucideIcon;
-  submenus: Submenu[];
+    href: string;
+    label: string;
+    active: boolean;
+    icon: LucideIcon;
+    submenus: Submenu[];
 };
 
 type Group = {
-  groupLabel: string;
-  menus: Menu[];
+    groupLabel: string;
+    menus: Menu[];
 };
 
 // Fonction pour créer un menu
 function createMenu(href: string, label: string, icon: LucideIcon, submenus: Submenu[], pathname: string): Menu {
-  return {
-    href,
-    label,
-    active: pathname.includes(href),
-    icon,
-    submenus
-  };
+    return {
+        href,
+        label,
+        active: pathname.includes(href),
+        icon,
+        submenus
+    };
 }
 
 // Fonction principale pour obtenir la liste des menus
 export function getMenuList(pathname: string): Group[] {
-  const menus = [
-    {
-      groupLabel: "",
-      href: "/",
-      label: "Accueil",
-      icon: Icons.home,
-      submenus: []
-    },
-    {
-      groupLabel: "",
-      href: "/our_spaces",
-      label: "Nos espaces",
-      icon: Icons.space,
-      submenus: [
-        { href: "/our_spaces", label: "Tous nos espaces", active: pathname === "/our_spaces" },
-        { href: "/our_spaces/private_offices", label: "Bureaux privés", active: pathname === "/our_spaces/private_offices" },
-        { href: "/our_spaces/open_space", label: "Open-space", active: pathname === "/our_spaces/open_space" },
-        { href: "/our_spaces/meeting_room", label: "Salle de réunion", active: pathname === "/our_spaces/meeting_room" },
-        { href: "/our_spaces/common_space", label: "Espaces Commun", active: pathname === "/our_spaces/common_space" },
-      ]
-    },
-    {
-      groupLabel: "",
-      href: "/our_offers",
-      label: "Nos offres",
-      icon: Icons.library,
-      submenus: [
-        { href: "/our_offers/domiciliation_virtuelle_entreprise", label: "Domiciliation virtuelle d'entreprise", active: pathname === "/our_offers/domiciliation_virtuelle_entreprise" },
-        { href: "/our_offers/domiciliation_physique_entreprise", label: "Domiciliation physique d'entreprise", active: pathname === "/our_offers/domiciliation_physique_entreprise" },
-        { href: "/our_offers/location_occasionnelle_de_bureaux_meubles", label: "Location occasionnelle de bureaux meublés", active: pathname === "/our_offers/location_occasionnelle_de_bureaux_meubles" },
-        { href: "/our_offers/location_salle_de_reunion", label: "Location salle de réunion", active: pathname === "/our_offers/location_salle_de_reunion" },
-        { href: "/our_offers/location_espace_evenementiel", label: "Location espace évènementiel", active: pathname === "/our_offers/location_espace_evenementiel" },
-        { href: "/our_offers/location_espace_tournage", label: "Location espace tournage", active: pathname === "/our_offers/location_espace_tournage" },
-      ]
-    },
-    {
-      groupLabel: "",
-      href: "/our_galeries",
-      label: "Galerie",
-      icon: Icons.galerie,
-      submenus: [
-        { href: "/our_galeries/evenements_collaboratifs", label: "Événements Collaboratifs", active: pathname === "/our_galeries/evenements_collaboratifs" },
-        { href: "/our_galeries/article_de_presse", label: "Article de Presse", active: pathname === "/our_galeries/article_de_presse" },
-        { label: "Événements à venir", active: pathname === "" }, // Pas de href
-      ]
-    },
-    {
-      groupLabel: "",
-      href: "/about",
-      label: "Qui sommes-nous ?",
-      icon: Icons.about,
-      submenus: []
-    },
-    {
-      groupLabel: "",
-      href: "/contact_us",
-      label: "Contactez-nous",
-      icon: Icons.phone,
-      submenus: []
-    },
-  ];
+    const menus = [
+        {
+            groupLabel: "",
+            href: "/",
+            label: "Accueil",
+            icon: Icons.home,
+            submenus: []
+        },
+        {
+            groupLabel: "",
+            href: "/our_spaces",
+            label: "Nos espaces",
+            icon: Icons.space,
+            submenus: [
+                { href: "/our_spaces", label: "Tous nos espaces", active: pathname === "/our_spaces" },
+                { href: "/our_spaces/private_offices", label: "Bureaux privés", active: pathname === "/our_spaces/private_offices" },
+                { href: "/our_spaces/open_space", label: "Open-space", active: pathname === "/our_spaces/open_space" },
+                { href: "/our_spaces/meeting_room", label: "Salle de réunion", active: pathname === "/our_spaces/meeting_room" },
+                { href: "/our_spaces/common_space", label: "Espaces Commun", active: pathname === "/our_spaces/common_space" },
+            ]
+        },
+        {
+            groupLabel: "",
+            href: "/our_offers",
+            label: "Nos offres",
+            icon: Icons.library,
+            submenus: [
+                { href: "/our_offers/domiciliation_virtuelle_entreprise", label: "Domiciliation virtuelle d'entreprise", active: pathname === "/our_offers/domiciliation_virtuelle_entreprise" },
+                { href: "/our_offers/domiciliation_physique_entreprise", label: "Domiciliation physique d'entreprise", active: pathname === "/our_offers/domiciliation_physique_entreprise" },
+                { href: "/our_offers/location_occasionnelle_de_bureaux_meubles", label: "Location occasionnelle de bureaux meublés", active: pathname === "/our_offers/location_occasionnelle_de_bureaux_meubles" },
+                { href: "/our_offers/location_salle_de_reunion", label: "Location salle de réunion", active: pathname === "/our_offers/location_salle_de_reunion" },
+                { href: "/our_offers/location_espace_evenementiel", label: "Location espace évènementiel", active: pathname === "/our_offers/location_espace_evenementiel" },
+                { href: "/our_offers/location_espace_tournage", label: "Location espace tournage", active: pathname === "/our_offers/location_espace_tournage" },
+            ]
+        },
+        {
+            groupLabel: "",
+            href: "/our_galeries",
+            label: "Galerie",
+            icon: Icons.galerie,
+            submenus: [
+                { href: "/our_galeries/evenements_collaboratifs", label: "Événements Collaboratifs", active: pathname === "/our_galeries/evenements_collaboratifs" },
+                { href: "/our_galeries/article_de_presse", label: "Article de Presse", active: pathname === "/our_galeries/article_de_presse" },
+                // Plain text entry
+                { label: "Événement à venir", active: false }, // No href, active is set to false
+            ]
+        },
+        {
+            groupLabel: "",
+            href: "/about",
+            label: "Qui sommes-nous ?",
+            icon: Icons.about,
+            submenus: []
+        },
+        {
+            groupLabel: "",
+            href: "/contact_us",
+            label: "Contactez-nous",
+            icon: Icons.phone,
+            submenus: []
+        },
+    ];
 
-  return menus.map(({ groupLabel, href, label, icon, submenus }) => ({
-    groupLabel,
-    menus: [createMenu(href, label, icon, submenus, pathname)]
-  }));
+    return menus.map(({ groupLabel, href, label, icon, submenus }) => ({
+        groupLabel,
+        menus: [createMenu(href, label, icon, submenus, pathname)]
+    }));
 }
