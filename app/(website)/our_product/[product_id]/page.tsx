df8e286a5c26de64fac559472f74b9ce0997188a
sylvayas <yasminesylva0758@gmail.com>
2025-04-28 22:03:08 +0000

    third commit

--- app/(website)/our_product/[product_id]/page.tsx
+++ app/(website)/our_product/[product_id]/page.tsx
@@ -1,13 +1,14 @@
 import HeaderPage from "@/components/offers/header-page";
 
 import DescriptionAllSpaces from "@/components/offers/DescriptionAllSpaces";
+import DescriptionAllSpacesprod from "@/components/product/DescriptionAllSpacesprod";
 
 export default async function ProductPage() {
   
   return (
     <div>
       <HeaderPage/>
-      <DescriptionAllSpaces />
+      <DescriptionAllSpacesprod />
       
     
     </div>
--- components/product/header-page.tsx
+++ components/product/header-page.tsx
@@ -12,9 +12,7 @@ export default function HeaderPage() {
                 <div className={cn("h-[80vh] md:h-[600px] xl:h-[450px] relative container px-0 max-w-[1400px]")}>
                     <div className="absolute z-[3] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center flex-col gap-8 max-w-5xl w-full">
                         <div className="max-w-screen-sm text-white text-center md:text-left">
-                            <h2 className="mb-4 text-xl sm:text-2xl md:text-3xl lg:text-4xl tracking-tight font-semibold">
-                                Besoin d&apos;un produit ? Commandez en ligne et recevez-le sans bouger de chez vous !
-                            </h2>
+                          
                             <p className="mb-8 font-light text-xs sm:text-sm">Bienvenue </p>
                         </div>
                      
